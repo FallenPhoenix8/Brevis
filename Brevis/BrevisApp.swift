b9fd6cf599634d4a76db04f01d818a31e9b5b694
FallenPhoenix8 <tplayxyz8@gmail.com>
2024-11-03 22:42:46 +0100

    Initial Commit

---
+++ Brevis/BrevisApp.swift
@@ -0,0 +1,17 @@
+//
+//  BrevisApp.swift
+//  Brevis
+//
+//  Created by Łukasz Kwiecień on 03/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct BrevisApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BrevisTests/BrevisTests.swift
@@ -0,0 +1,16 @@
+//
+//  BrevisTests.swift
+//  BrevisTests
+//
+//  Created by Łukasz Kwiecień on 03/11/2024.
+//
+
+import Testing
+
+struct BrevisTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
